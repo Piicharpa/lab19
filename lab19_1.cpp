cb7025582b86d76dd06ed2dd787e84c097c7204b
Piicharpa <neuah2515@gmail.com>
2023-02-17 18:03:46 +0700

    complete

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,76 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string fn, vector<string> &names, vector<int> &scores, vector<char> &grades){
+    ifstream source(fn.c_str());
+    string s ;
+    while(getline(source,s)){
+    
+    char format[] = "%[^:]: %d %d %d";
+    char name[100];
+    int a,b,c;
+    sscanf(s.c_str(),format,name, &a, &b, &c);
+    names.push_back(name);
+    scores.push_back(a+b+c);
+    grades.push_back(score2grade(a+b+c));
+    }
 }
 
-void getCommand(){
-
+ void getCommand(string &command, string &key){
+    cout << "Please input your command: ";
+    string temp;
+    getline(cin, temp);
+    int j = temp.size();
+    int start = 0;
+    
+    int end = temp.find_first_of(" ");
+    
+    command = temp.substr(start, end - start);
+    key = temp.substr(end+1, j-1);
+    
+    
 }
 
-void searchName(){
-
+void searchName(vector<string> name, vector<int> score, vector<char> grade, string key){
+     cout << "---------------------------------\n";
+    
+    int x=0;
+    
+   
+    for(unsigned int i=0 ;i<name.size();i++){
+        if(key == toUpperStr(name[i])){
+        
+        cout << name[i] << "'s score = " << score[i] << "\n";
+        cout << name[i] << "'s grade = " << grade[i] << "\n";
+        
+    
+        x++;
+        }else if(i == name.size()-1 && x==0){
+            cout << "Cannot found." << endl;
+        }
+    }
+    
+    cout << "---------------------------------\n";
+    
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> name, vector<int> score, vector<char> grade, string key){
+     int x=0;
+    
+    cout << "---------------------------------\n";
+    
+    for(unsigned int i=0 ;i<name.size();i++){
+    string str(1,grade[i]);
+        if(key == str){
+        cout << name[i] << " (" << score[i] << ")\n"; 
+      
+        x++;
+        }else if(i == name.size()-1 && x==0){
+            cout << "Cannot found." << endl;
+        }
+    }
+    
+    cout << "---------------------------------\n";
 }
 
 
